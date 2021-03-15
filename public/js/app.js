a5c92f1bc435f7396007709eb36fa07485d30872
phucnguyen070995 <p070995@gmail.com>
2021-03-15 13:09:50 +0700

    add humidity

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,10 @@
 // console.log('hello console')
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
-    })
-})
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then((data) => {
+//         console.log(data)
+//     })
+// })
 
 
 const weatherForm = document.querySelector('form')
--- utils/forecast.js
+++ utils/forecast.js
@@ -8,7 +8,11 @@ const forecast = (latitude, longtitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location. Try another search!', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is curently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+            callback(
+                undefined, 
+                body.current.weather_descriptions[0] + '. It is curently ' + body.current.temperature + ' degrees out. It feels like ' + 
+                body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity + ' %.'
+            )
         }
     })
 }
